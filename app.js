caf1ae5b27da7277c2d36334ffdaaa73dd77ea00
Leah Treidler <leahtreidler@Leahs-MacBook-Pro.local>
2020-07-28 13:27:34 -0700

    Logs names of all similar cities

--- app.js
+++ app.js
@@ -1,36 +1,21 @@
-//import bodyParser from 'body-parser';
-// import axios from 'axios';
-//import express from 'express';
-
-
-
-    console.log('go');
-// const getUAs = function(search) {   
-//     var request = new XMLHttpRequest()
-
-//     request.open('GET', 'https://api.teleport.org/api/urban_areas', true);
-    
-//     request.onload = function() {
-//         var cities = (JSON.parse(this.response));
-        
-//         if (request.status >= 200 && request.status < 400) {
-//             cities = cities._links["ua:item"];
-//             cities.forEach(city => {
-//                 (city.name).includes(search) ? console.log(city) : null;
-//             });
-//         } else {
-//             console.log('error');
-//         }
-//     }
-    
-//     request.send();
-// }
-
+//WORKING HERE
+async function loopCities(callback) {
+    let cities = await makeReq('https://api.teleport.org/api/urban_areas');
+    const results = [];
+    cities = cities._links["ua:item"];
+    for(let i=0; i<cities.length; i++){
+        let bool = await callback(cities[i]);
+        if(bool){
+            results.push(cities[i]);
+            console.log(cities[i].name);
+        }
+    }
+    return results;
+}
 
 
 function makeReq(url) {
     let data;
-
     return new Promise( (resolve, reject) => {
         var request = new XMLHttpRequest()
 
@@ -39,7 +24,6 @@ function makeReq(url) {
         request.onload = function() {
             data = (JSON.parse(this.response));
             if (request.status >= 200 && request.status < 400) {
-                
                 resolve(data);
                 return data;
             } else {
@@ -47,28 +31,77 @@ function makeReq(url) {
                 reject();
             }
         }  
-
         request.send(); 
-
     });
 }
 
-async function getUAs(search) {    
-    let results = [];
-    try {
-        let cities = await makeReq('https://api.teleport.org/api/urban_areas');
-        cities = cities._links["ua:item"];
-        cities.forEach(city => {
-            if((city.name).includes(search)){
-                results.push(city);
-            }
-        });
-        results.length === 0 ? console.log('No results') : console.log(results);
 
+
+async function getUAs(search) {    
+    try {
+        //loops through all the cities and returns an array of cities with matching names 
+        const results = await loopCities(city => city.name.includes(search) ? true : false);
+        
+        if(results.length === 0){
+            console.log("No results.");
+        } else if (results.length > 1){
+            console.log("Choose an area");
+            results.forEach(city => console.log(city));
+        } else {
+            console.log(results[0].name);
+            const score = await getScore(results[0]);
+            //find all cities with similar scores
+            console.log(score);
+            const similarCities = await loopCities((city) => isSimilar(city, score));
+            console.log(similarCities);
+        }
     } catch(error) {
         console.log(error);
     }
-    
+}
+
+async function isSimilar(city, score){
+    //await the city's score
+    let cScore = await getScore(city);
+    //if the score is in the right range, then return true
+    if(cScore > (score - 1) && cScore < (score + 1) && cScore != score){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+async function getScore(cityObj) {
+    //given a city object, returns a score
+    const link = `${cityObj.href}scores/`
+    let score;
+        try {
+            score = await makeReq(link);
+            //get the results from the href   
+            return score.teleport_city_score;
+
+        } catch (error) {
+            console.log(error);
+        }
+}   
+
+async function getSimilarScore(score) {
+    console.log('Finding similar score: ');
+    try {
+        let cities = await makeReq('https://api.teleport.org/api/urban_areas');
+        //for each ._links["ua:item"] , get the score 
+        console.log("Cities "+cities);
+        cities.forEach(city => {
+            console.log(getScore(city));
+            if( getScore(city) > score - 5 && getScore(city) < score + 5){
+                console.log("Similar Score: "+city);
+            };
+        });
+
+    } catch {
+
+    }
 }
 
 
